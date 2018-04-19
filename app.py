f43fccfbb3acf3951e6f7f634774e4b5bc8f3fab
dasa <leandro.costa.exta@dasa.com.br>
2018-04-18 23:58:01 -0300

    Configure init app first

--- app.py
+++ app.py
@@ -32,11 +32,5 @@ if __name__ == "__main__":
     from utils.database import db
 
     db.init_app(app)
-
-
-    @app.before_first_request
-    def create_tables():
-        db.create_all()
-
-
+    db.create_all()
     app.run()
